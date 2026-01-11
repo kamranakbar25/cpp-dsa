08dd8ebfc7ec4a0636edc5de31921e1936ba2827
Kamran Akbar <findsorzo@gmail.com>
2026-01-11 23:46:49 +0530

    Add some code in Function

--- fundamentals/04_function.cpp
+++ fundamentals/04_function.cpp
@@ -1,7 +1,37 @@
 #include<iostream>
 using namespace std;
 
-int main(){
 
+// Sum of 2 nums.
+int sum(int a, int b){
+    int result = a + b;
+    return result;
+}
+
+// Find min of 2 nums.
+int minNum(int a, int b){
+    if (a > b){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
+
+// Calculate the sum of numbers from 1 to N.
+int sumNum(int n){
+    int givenNum = n;
+    int sum = 0;
+    for (int i = 0; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+
+int main(){
+    cout << sum(3, 7) << endl;
+    cout << "Min num: " << minNum(3, 1) << endl;
+    cout << "Total sum: " << sumNum(4) << endl;
     return 0;
 }
