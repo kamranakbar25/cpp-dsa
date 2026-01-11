e59e11172cade31cb2815ec5407d2c3d23a0a75c
Kamran Akbar <findsorzo@gmail.com>
2026-01-12 00:30:05 +0530

    Add some more problems

--- fundamentals/04_function.cpp
+++ fundamentals/04_function.cpp
@@ -19,19 +19,50 @@ int minNum(int a, int b){
 }
 
 // Calculate the sum of numbers from 1 to N.
-int sumNum(int n){
-    int givenNum = n;
+int sumNum(int givenNum){
     int sum = 0;
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i <= givenNum; i++){
         sum += i;
     }
     return sum;
+}
+
+// Calculate N factorial.
+int factNum(int factNum){
+    int mulNum = 1;
+    for (int i = 1; i <= factNum; i++){
+        mulNum *= i;
+    }
+    return mulNum;
+}
+
+// Calculate sum of digits of a number.
+int digSum(int dgts){
+    int digitSum = 0;
+    while (dgts > 0){
+        int lastDig = dgts % 10;
+        dgts /= 10;
+        digitSum += lastDig;
+    }
+    return digitSum;
+}
+
+// Calculate nCr binomial coefficient for n & r.
+int nCr(int n, int r){
+    int factOfn = factNum(n);
+    int factOfr = factNum(r);
+    int factOfnr = factNum((n-r));
+    int result = factOfn/(factOfr * factOfnr);
+    return result;
 }
 
 
 int main(){
     cout << sum(3, 7) << endl;
     cout << "Min num: " << minNum(3, 1) << endl;
-    cout << "Total sum: " << sumNum(4) << endl;
+    cout << "Total sum: " << sumNum(10) << endl;
+    cout << "Factorial: " << factNum(5) << endl;
+    cout << "Sum of digits of num: " << digSum(1453) << endl;
+    cout << "nCr: " << nCr(8, 2) << endl;
     return 0;
 }
