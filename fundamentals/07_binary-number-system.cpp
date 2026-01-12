2be9651bf3513977ef37a5418551742186d67820
Kamran Akbar <findsorzo@gmail.com>
2026-01-12 15:24:37 +0530

    Decimal to binary problem

--- fundamentals/07_binary-number-system.cpp
+++ fundamentals/07_binary-number-system.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
+
+// Decimal to binary
+int decToBin(int decNum){
+    int ans = 0, power = 1;
+    while (decNum > 0){
+        int binNum = decNum % 2;
+        decNum /= 2;
+        ans += (binNum * power);
+        power *= 10;
+    }
+    return ans;
+}
 int main(){
+    cout << decToBin(50) << endl;
 
 
     return 0;
