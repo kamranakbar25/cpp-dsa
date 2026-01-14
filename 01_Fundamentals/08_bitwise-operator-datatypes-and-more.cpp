14d5540dfad87bd617bec60d36135fa5a960fb5d
Kamran Akbar <findsorzo@gmail.com>
2026-01-14 22:07:12 +0530

    Bitwise operator

--- 01_Fundamentals/08_bitwise-operator-datatypes-and-more.cpp
+++ 01_Fundamentals/08_bitwise-operator-datatypes-and-more.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
-
+    cout << (4 & 3) << endl;
+    cout << (4 | 8) << endl;
+    cout << (4 ^ 9) << endl;
+    cout << (10 << 2) << endl;
+    cout << (8 >> 2) << endl;
     return 0;
 }
