38ca3e84bda51de107905c22f771fb861b53ee01
Kamran Akbar <findsorzo@gmail.com>
2026-01-16 00:23:08 +0530

    Some changes in array

--- 02_Array/array-data-structure.cpp
+++ 02_Array/array-data-structure.cpp
@@ -3,84 +3,84 @@
 using namespace std;
 
 int main(){
-    // int marks[5] = {99, 89, 76, 68, 93};
-    // double price[] = {199.99, 59.89, 100.99};
+    int marks[5] = {99, 89, 76, 68, 93};
+    double price[] = {199.99, 59.89, 100.99};
 
-    // cout << marks[0] << endl;
+    cout << marks[0] << endl;
 
-    // marks[0] = 72;
+    marks[0] = 72;
 
-    // cout << marks[0] << endl;
-    // cout << marks[1] << endl;
-    // cout << marks[2] << endl;
-    // cout << marks[3] << endl;
-    // cout << marks[4] << endl;
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
 
-    // cout << endl;
+    cout << endl;
 
-    // cout << price[0] << endl;
-    // cout << price[1] << endl;
-    // cout << price[2] << endl;
+    cout << price[0] << endl;
+    cout << price[1] << endl;
+    cout << price[2] << endl;
 
-    // cout << endl;
+    cout << endl;
 
-    // // Find size of an array:
-    // cout << sizeof(marks) / sizeof(int) << endl;
+    // Find size of an array:
+    cout << sizeof(marks) / sizeof(int) << endl;
 
-    // cout << endl;
+    cout << endl;
 
-    // // Loop on arrays: 0 to size - 1.
-    // int size = 5;
-    // for (int i = 0; i < size; i++){
-    //     cout << marks[i] << endl;
-    // }
+    // Loop on arrays: 0 to size - 1.
+    int size = 5;
+    for (int i = 0; i < size; i++){
+        cout << marks[i] << endl;
+    }
 
-    // cout << endl;
+    cout << endl;
 
-    // // Taking array input using loops.
-    // int arrSize = 5;
+    // Taking array input using loops.
+    int arrSize = 5;
 
-    // int mainArray[arrSize];
-    // for (int i = 0; i < arrSize; i++){
-    //     cin >> mainArray[i];
-    // }
-    // for (int i = 0; i < arrSize; i++){
-    //     cout << mainArray[i] << endl;
-    // }
+    int mainArray[arrSize];
+    for (int i = 0; i < arrSize; i++){
+        cin >> mainArray[i];
+    }
+    for (int i = 0; i < arrSize; i++){
+        cout << mainArray[i] << endl;
+    }
 
-    // cout << endl;
+    cout << endl;
 
     // Find smallest/largest in an array.
-    // int intArr[5] = {56, 23, 65, -12, 78};
-    // int sizeArr = 5;
-    // int smallest = INT_MAX;
-    // int largest = INT_MIN;
+    int intArr[5] = {56, 23, 65, -12, 78};
+    int sizeArr = 5;
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
 
-    // // Method 1.
-    // for (int i = 0; i < sizeArr; i++){
-    //     if (intArr[i] < smallest){
-    //         smallest = intArr[i];
-    //     }
-    // }
-    // for (int i = 0; i < sizeArr; i++){
-    //     if (intArr[i] > largest){
-    //         largest = intArr[i];
-    //     }
-    // }
-    // cout << "Smallest num: " << smallest << endl;
-    // cout << "Largest num: " << largest << endl;
+    // Method 1.
+    for (int i = 0; i < sizeArr; i++){
+        if (intArr[i] < smallest){
+            smallest = intArr[i];
+        }
+    }
+    for (int i = 0; i < sizeArr; i++){
+        if (intArr[i] > largest){
+            largest = intArr[i];
+        }
+    }
+    cout << "Smallest num: " << smallest << endl;
+    cout << "Largest num: " << largest << endl;
 
-    // cout << endl;
+    cout << endl;
 
-    // // Method 2.
-    // for (int i = 0; i < sizeArr; i++){
-    //     smallest = min(intArr[i], smallest);
-    //     largest = max(intArr[i], largest);
-    // }
-    // cout << "Smallest num: " << smallest << endl;
-    // cout << "Largest num: " << largest << endl;
+    // Method 2.
+    for (int i = 0; i < sizeArr; i++){
+        smallest = min(intArr[i], smallest);
+        largest = max(intArr[i], largest);
+    }
+    cout << "Smallest num: " << smallest << endl;
+    cout << "Largest num: " << largest << endl;
 
-    // cout << endl;
+    cout << endl;
 
 
     // Find smallest/largest element's index of an array.
